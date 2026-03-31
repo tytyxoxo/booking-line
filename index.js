2051d085baa658d33d379a977522bfa5028ca78e
Passon Rattanakongton <passon.r@nutritionprofess.com>
2026-03-31 13:33:14 +0700

    added env

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ const client = new line.messagingApi.MessagingApiClient({
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
 });
 const auth = new google.auth.GoogleAuth({
-  credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS),
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  },
   scopes: ["https://www.googleapis.com/auth/calendar"],
 });
+
 const calendar = google.calendar({ version: "v3", auth });
 const CALENDAR_ID = process.env.GOOGLE_CALENDAR_ID;
 
